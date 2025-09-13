be56d588a60cb4093258b33d20df437804c993d7
Yevheniia <tobolovskaya@gmail.com>
2025-09-13 08:39:08 +0200

    Remove lesson 5 Terraform infrastructure files and associated documentation

--- lesson-5/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "my-terraform-state-bucket-lesson5"
-    key            = "lesson-5/terraform.tfstate"
-    region         = "us-west-2"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
-  }
-}
