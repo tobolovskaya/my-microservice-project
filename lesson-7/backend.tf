0da32d0faed6337f1d8a222be337c72153d2feb1
Yevheniia <tobolovskaya@gmail.com>
2025-09-13 09:18:04 +0200

    Remove Django application Helm chart and associated Terraform infrastructure
    - Deleted Helm chart files for the Django application including Chart.yaml, templates, and values.yaml.
    - Removed Terraform configuration files for ECR, EKS, VPC, and S3 backend modules.
    - Eliminated scripts for deployment and cleanup of the Django application and its resources.

--- lesson-7/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "my-terraform-state-bucket-lesson7"
-    key            = "lesson-7/terraform.tfstate"
-    region         = "us-west-2"
-    dynamodb_table = "terraform-locks-lesson7"
-    encrypt        = true
-  }
-}
