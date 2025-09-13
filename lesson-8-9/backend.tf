805d2ac4e8980024cc6ce95c497ca068f76ea3c1
Yevheniia Tobolovska <142045878+tobolovskaya@users.noreply.github.com>
2025-09-13 09:34:28 +0200

    Deleted backend.tf

--- lesson-8-9/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "my-terraform-state-bucket-lesson8-9"
-    key            = "lesson-8-9/terraform.tfstate"
-    region         = "us-west-2"
-    dynamodb_table = "terraform-locks-lesson8-9"
-    encrypt        = true
-  }
-}
